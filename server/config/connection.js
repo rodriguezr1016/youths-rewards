a1c6b8a69c9d07b0403dae54423bd62c4385b2d5
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2024-03-31 02:30:56 +0000

    updating

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/youths-reward');
 
 module.exports = mongoose.connection;
+
+// username: rodriguezr1016 password:WHP9zzhZU_bh9j2
